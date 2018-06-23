18d24306077bf643f087c05e287a318050c41da6
Pierre Lamusse <p.lamusse@hotmail.fr>
2018-06-23 17:31:29 +0200

    starting parsing

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -1,11 +1,31 @@
 #ifndef LEM_IN_H
 # define LEM_IN_H
+
+/*
+****** INCLUDE ******
+*/
+
 # include "../libft/includes/libft.h"
-typedef struct		s_env
+# include "../libft/includes/get_next_line.h"
+# include "../libft/includes/ft_printf.h"
+
+/*
+****** DEFINE ******
+*/
+
+# define INIT_ZERO		0
+# define END_OF_FILE	0
+# define SUCCESS		0
+# define ERROR			-1
+
+/*
+****** STRUCT ******
+*/
+
+typedef struct	s_hex
 {
-	int	fd;
 	int	ants;
-	
-	
-}			t_env;
+	int	step;
+}				t_hex;
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamusse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/20 17:46:24 by plamusse          #+#    #+#             */
+/*   Updated: 2018/06/23 17:28:02 by plamusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem-in.h"
 
-int	main(int argc, char **argv)
+int		parse_hex(t_hex *env)
 {
-	int	ret;
+	char	*line = NULL;
+	int		ret;
 
-	if (argc != 2)
+	ret = SUCCESS;
+	(void)env;
+	while (ret != ERROR && get_next_line(STDIN_FILENO, &line) > END_OF_FILE)
 	{
-		ft_printf("ERROR\n");
-		return (0);
+		if (*line == '#')
+		{
+			if (*(line + 1) == '#')
+				ret = SUCCESS; //handle_command(env, line);
+			else
+				ft_printf("%s\n", line);
+		}
+		//else if (*line == 'L')
+		//	ret == ERROR;
+		free(line);
 	}
-	ret = 	
+	return (ret);
+}
+
+void	init_hex(t_hex *env)
+{
+	env->ants = INIT_ZERO;
+	env->step = INIT_ZERO; 
+}
+
+int		main(void)
+{
+	t_hex	env;
+
+	init_hex(&env);
+	parse_hex(&env);
 	return (0);
 }
